876ea884515483caa74a536ae2239e6026de204e
Sergey <noka96@icloud.com>
2023-07-05 11:34:46 +0700

    Lesson and pracktic

--- js/script.js
+++ js/script.js
@@ -1,79 +1,96 @@
 "use strict"
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-
-start();
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     ganres: [],
-    privat: false
-}
-
-
-
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцение его?', '');
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
     
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
+                  b = prompt('На сколько оцение его?', '');
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log('Просмотрено довольно мало фильмов');   
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('Вы классный зритель'); 
+        } else if (personalMovieDB.count >= 30) {
+            console.log('Вы киноман'); 
         } else {
-            console.log('error');
-            i--;
+            console.log('Произошла ошибка'); 
+        }    
+    
+    },
+
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+
+    wtiteYourGeners: function() {
+        for (let i = 1; i <= 3; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+            if(genre == null || genre === ''){
+                console.log("Вы ввели не коректные данные");
+                i--;
+                
+            } else {
+                personalMovieDB.ganres[i - 1] = genre;
+            }   
+        }
+
+    personalMovieDB.ganres.forEach((item, i) => {
+        console.log(`Любимый жанр ${i+1} - это ${item}`);
+    });  
+    },
+
+    toggleVisibleMyDB: function() {
+        if(personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
         }
     }
-}
 
-//rememberMyFilms();
-
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('Просмотрено довольно мало фильмов');   
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log('Вы классный зритель'); 
-    } else if (personalMovieDB.count >= 30) {
-        console.log('Вы киноман'); 
-    } else {
-        console.log('Произошла ошибка'); 
-    }    
-
-}
-
-//detectPersonalLevel();
+};
 
 
 
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
 
 
 
-showMyDB(personalMovieDB.privat);
 
 
-function wtiteYourGeners() {
-    for (let i = 1; i <= 3; i++) {
-        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
-        personalMovieDB.ganres[i - 1] = genre;
-    }
-}
 
-wtiteYourGeners();
+
+
+
+
+
+
+
+
+
+
--- script2.js
+++ script2.js
@@ -1,26 +1,156 @@
-"use strict";
-
-const str = 'test';
+'use strict';
 
 
+// Урок 33.
+// const arr = [1, 22, 23, 16, 8];
 
-console.log(str.toUpperCase());
-console.log(str.toLocaleLowerCase());
-console.log(str);
+// arr.sort(compareNan);
+// console.log(arr);
 
 
-const fruit = 'Some fruit';
+// function compareNan(a, b) {
+//     return a - b;
+// }
+// arr[99] = 0
+// console.log(arr.length);
 
-console.log(fruit.indexOf('fruit'));
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
 
-const logg = 'Hello world';
+// });
 
-console.log(logg.slice(6, 11));
 
-console.log(logg.substr(6, 2));
 
-const num = 12.2;
-console.log(Math.round(num));
+// const str = prompt('',"");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join('; '));
 
-const test = '12.2px';
-console.log(parseFloat(test));
+// Урок 35 Клонирование объектов
+
+// let a = 5, //в приметивных данных объекты могут заменяться
+//     b = a;
+
+// b = b + 5;
+
+// console.log(b);
+// console.log(a);
+
+// const  obj = { //сдесь идет передача по ссылке, а модифицируя копию ты модефецируешь объект
+//     a: 5,
+//     b: 1
+// };
+
+// const copy = obj; 
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+// function copy(mainObj) {
+//     let objCopy = {};
+
+//     let key;
+//     for(key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+
+//     return objCopy;
+// }
+
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers. a = 10;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+
+// const clone = Object.assign({}, add);
+
+// clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'ffdsd';
+
+// console.log(newArray);
+// console.log(oldArray);
+
+
+//Оператор разворота
+
+// const video = ['youtube', 'rutube', 'mail'],
+//       blogs = ['wordpress','livejournal', 'blogger'],
+//       internet = [...video, ...blogs, 'vk', 'facebook'];
+
+// console.log(internet);
+
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2, 5, 7];
+
+// log(...num);
+
+// const array = ['a', 'b'];
+
+// const newAarray = [...array];
+
+// const q = {
+//     one: 1,
+//     two: 2
+// };
+
+// const newObj = {...q};
+
+// console.log(newObj);
+// console.log(newAarray);
+
+
+//Урок 36. ООП прототипно ориентированное программирование
+
+// const soldier = {
+//     health: 400,
+//     armor: 100,
+//     sayHello: function() {
+//         console.log('Hello')
+//     }
+// };
+
+// // // const jonh = {
+// //     health: 100
+
+// // };
+
+// const jonh = Object.create(soldier); // создание прототипа солдат = джон
+
+// // тоже самое что и ниже jonh.__proto__= soldier;
+
+// // Object.setPrototypeOf(jonh, soldier);
+
+// console.log(jonh.armor);
+// jonh.sayHello();
+
+// 37 Урок
